fd4ae5513153cd044b04064cdff768c44cc1bbb0
seankearon <sean@tackaberry.org>
2020-11-09 08:21:57 +0000

    Added base projects.

---
+++ EntryPointAPI/Controllers/CustomerController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace EntryPointAPI.Controllers
+{
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+        private readonly ILogger<CustomerController> _logger;
+
+        public CustomerController(ILogger<CustomerController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost]
+        [Route("newcustomer")]
+        public IActionResult NewCustomer()
+        {
+            return Ok();
+        }
+    }
+}
