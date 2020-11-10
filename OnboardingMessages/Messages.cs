1e299c5b21e393557ff138ae489a4a929bac909f
seankearon <sean@tackaberry.org>
2020-11-10 08:22:55 +0000

    Actually added the messages!

---
+++ OnboardingMessages/Messages.cs
@@ -0,0 +1,29 @@
+﻿namespace OnboardingMessages
+{
+    public record CreateCustomerAccount
+    {
+        public string Name  { get; init; }
+        public string Email { get; init; }
+    }
+
+    public record CustomerAccountCreated
+    {
+        public string Email      { get; init; }
+        public int    CustomerId { get; init; }
+    }
+
+    public record SendWelcomeEmail
+    {
+        public int CustomerId { get; init; }
+    }
+
+    public record ScheduleSalesCall
+    {
+        public int CustomerId { get; init; }
+    }
+
+    public record CustomerOnboarded
+    {
+        public int CustomerId { get; init; }
+    }
+}
