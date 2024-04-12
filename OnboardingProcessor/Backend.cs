e41a3a203cff8039763527403adc99613a370408
seankearon <sean@tackaberry.org>
2024-04-12 13:38:34 +0100

    Removed unused reference.

--- OnboardingProcessor/Backend.cs
+++ OnboardingProcessor/Backend.cs
@@ -3,7 +3,6 @@ using System.Threading.Tasks;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Rebus.Bus;
-using Rebus.ServiceProvider;
 
 namespace OnboardingProcessor
 {
